def1a34db31e0f5fa1fe29db5e9055ec09050379
Ebuka <ebukaakeru@manqala.com>
2022-02-04 14:48:59 +0100

    Sungas End of day

---
+++ sungas/sungas/report/end_of_day_report/end_of_day_report.js
@@ -0,0 +1,50 @@
+// Copyright (c) 2016, Manqala and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["End of Day Report"] = {
+	"filters": [
+		{
+			fieldname:"from_date",
+			label: __("From Date"),
+			fieldtype: "Date",
+			default: new Date(),
+			reqd: 1
+		},
+		{
+			fieldname:"to_date",
+			label: __("To Date"),
+			fieldtype: "Date",
+			default: new Date(),
+			reqd: 1
+		},
+		{
+			fieldname:"customer",
+			label: __("Customer"),
+			fieldtype: "Link",
+			options:"Customer",
+			reqd: 0
+		},
+		{
+			fieldname:"warehouse",
+			label: __("Warehoue"),
+			fieldtype: "Link",
+			options:"Warehouse",
+			reqd: 0
+		},
+		{
+			fieldname:"item_group",
+			label: __("Item Group"),
+			fieldtype: "Link",
+			options:"Item Group",
+			reqd: 0
+		},
+		{
+			fieldname:"pos_profile",
+			label: __("Store"),
+			fieldtype: "Link",
+			options:"POS Profile",
+			reqd:0
+		},
+	]
+};
