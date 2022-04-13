7d1c0d7ba42063ab6873e44d550c3f7dc0527d2e
Ebuka <ebukaakeru@manqala.com>
2022-04-13 09:49:20 +0100

    Inherit naming series from parent

--- sungas/utils/utils.py
+++ sungas/utils/utils.py
@@ -5,6 +5,8 @@ from frappe.utils import strip
 
 def item_name(doc,ev):
     if frappe.db.get_default("item_naming_by") == "Naming Series":
+        if doc.variant_of:
+            doc.naming_series = frappe.db.get_value("Item",doc.variant_of,'naming_series')
         from frappe.model.naming import set_name_by_naming_series
         set_name_by_naming_series(doc)
         doc.item_code = doc.name
